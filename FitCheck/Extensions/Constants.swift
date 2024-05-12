a0ff1d0afa15cb9fb26a711889ebf41e3af26804
albhandari <alexbhandari37@gmail.com>
2024-05-11 17:32:49 -0700

    api update

--- FitCheck/Extensions/Constants.swift
+++ FitCheck/Extensions/Constants.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum Constants{
-//    static let chatBotUrl = "https://api.openai.com/v1/chat/completions"
-//    static let apiKey = "INSERT HERE"
+    static let chatBotUrl = "https://api.openai.com/v1/chat/completions"
+    static let apiKey = "INSERT HERE"
     
 }
 
